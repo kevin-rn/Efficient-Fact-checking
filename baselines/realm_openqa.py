b12d7e3a8d95893ea66a6051de9dd969da4ef90a
Kevin Nanhekhan <k.r.nanhekhan@student.tudelft.nl>
2023-10-18 13:42:14 +0200

    Update corpus construction and realm qa scripts

--- baselines/realm_openqa.py
+++ baselines/realm_openqa.py
@@ -7,11 +7,10 @@ df_wiki_dev = pd.read_json('dev.json')
 corpus_data = np.load('wikimusique_corpus.npy', allow_pickle=True)
 
 tokenizer = AutoTokenizer.from_pretrained("google/realm-orqa-nq-openqa")
-retriever = RealmRetriever.from_pretrained("google/realm-orqa-nq-openqa", block_records=corpus_data)
+retriever = RealmRetriever(block_records=corpus_data, tokenizer=tokenizer)
 config = RealmConfig()
 config.num_block_records = len(corpus_data)
 model = RealmForOpenQA.from_pretrained("google/realm-orqa-nq-openqa", config=config, retriever=retriever, ignore_mismatched_sizes=True)
-
 
 predicted_vals = []
 loss_vals = []
@@ -26,12 +25,15 @@ for i in tqdm(range(len(df_wiki_dev))):
         return_attention_mask=False,
     ).input_ids
 
-    reader_output, predicted_answer_ids = model(**question_ids, answer_ids=answer_ids, return_dict=False)
-    predicted_answer = tokenizer.decode(predicted_answer_ids)
-    loss = reader_output.loss
+    try:
+        reader_output, predicted_answer_ids = model(**question_ids, answer_ids=answer_ids, return_dict=False)
+        predicted_answer = tokenizer.decode(predicted_answer_ids)
+        loss = reader_output.loss
 
-    predicted_vals.append(predicted_answer)
-    loss_vals.append(loss)
+        predicted_vals.append(predicted_answer)
+        loss_vals.append(loss)
+    except Exception as e:
+        print(f"Error processing question: {question} , with error: {e}")
 
 df_wiki_dev['predicted'] = predicted_vals
 df_wiki_dev['loss'] = loss_vals
